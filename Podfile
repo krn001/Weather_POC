5459ba84c8fae61e76c5936f7a4c5d7c3f830c90
Mac <mac@myMac.local>
2019-04-01 14:06:47 +0530

    Project created

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '10.2' 
+
+target 'Weather_POC' do
+  
+use_frameworks!
+
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+  pod 'SVProgressHUD'
+  
+end
---
+++ Weather_POC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather_POC
+//
+//  Created by mac on 01/04/19.
+//  Copyright © 2019 kiranJuware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
